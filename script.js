83947fdb3be3522976d362251f3f0c1410c2f26d
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-10-04 11:06:28 +0900

    90% code finish

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const clearAll = document.getElementById("clearAll");
 const filter = document.getElementById("filter");
-const items = document.querySelectorAll("li");
-
-// funciton add gareko
 function addItem(e) {
   e.preventDefault();
 
@@ -15,6 +12,13 @@ function addItem(e) {
     return;
   }
 
+  DOMmaItemadd(item);
+  addItemtoStorage(item);
+
+  checkPage();
+}
+
+function DOMmaItemadd(item) {
   const text = document.createTextNode(item);
   const li = document.createElement("li");
   const btn = document.createElement("button");
@@ -27,28 +31,22 @@ function addItem(e) {
   btn.appendChild(icon);
   itemList.appendChild(li);
   itemInput.value = "";
-
-  checkPage();
 }
-// remove gareko item
 function removeItem(e) {
   const btn = e.target.parentElement;
-
   if (btn.classList.contains("cross")) {
     btn.parentElement.remove();
   }
   checkPage();
 }
-// all item clear gareko
+
 function clearAllItem() {
   itemList.innerHTML = "";
-
   checkPage();
 }
 
 function checkPage() {
   const items = document.querySelectorAll("li");
-  console.log(items);
   if (items.length === 0) {
     filter.style.display = "none";
     clearAll.style.display = "none";
@@ -65,16 +63,46 @@ function filterItems(e) {
   items.forEach((item) => {
     console.log(item.firstChild.textContent.toLowerCase());
     const itemName = item.firstChild.textContent.toLowerCase();
-    if (itemName.indexOf(value !== -1)) {
+    if (itemName.indexOf(value) !== -1) {
       item.style.display = "flex";
     } else {
-      items.style.display = "none";
+      item.style.display = "none";
     }
   });
 }
 
+function addItemtoStorage(item) {
+  let itemsFromLocalStorage = getItemsFromLocalStorage();
+  itemsFromLocalStorage.push(item);
+  localStorage.setItem("items", JSON.stringify(itemsFromLocalStorage));
+  console.log(itemsFromLocalStorage);
+}
+
+function getItemsFromLocalStorage() {
+  let itemsFromLocalStorage = localStorage.getItem("items");
+  console.log(typeof itemsFromLocalStorage);
+  if (itemsFromLocalStorage === null) {
+    itemsFromLocalStorage = []; //empty array
+    console.log("no");
+  } else {
+    itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"));
+    console.log(itemsFromLocalStorage);
+    console.log(typeof itemsFromLocalStorage);
+  }
+  return itemsFromLocalStorage;
+}
+
+function displayonDOM() {
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //banana,milk
+  itemsFromLocalStorage.forEach((item) => {
+    DOMmaItemadd(item);
+  });
+}
+displayonDOM();
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearAll.addEventListener("click", clearAllItem);
-filter.addEventListener("click", filterItems);
+filter.addEventListener("input", filterItems);
+
 checkPage();
